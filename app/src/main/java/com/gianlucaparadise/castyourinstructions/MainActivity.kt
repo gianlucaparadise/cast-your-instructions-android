d8ef1627ee2bbf9050f0fe452d89eb1eb064a6b5
gianlucaparadise <gianlucaparadise@gmail.com>
2019-09-07 09:07:44 +0200

    Added cast button

--- app/src/main/java/com/gianlucaparadise/castyourinstructions/MainActivity.kt
+++ app/src/main/java/com/gianlucaparadise/castyourinstructions/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.gianlucaparadise.castyourinstructions
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.cast.framework.CastButtonFactory
+import com.google.android.gms.cast.framework.CastContext
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mCastContext: CastContext? = null
+    private var mediaRouteMenuItem: MenuItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mCastContext = CastContext.getSharedInstance(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main, menu)
+        mediaRouteMenuItem =
+            CastButtonFactory.setUpMediaRouteButton(applicationContext, menu, R.id.media_route_menu_item)
+        return true
     }
 }
