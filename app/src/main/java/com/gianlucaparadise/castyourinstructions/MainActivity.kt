792517a564dd7e35dcc59d3f17f2cc485ebafa37
gianlucaparadise <gianlucaparadise@gmail.com>
2019-09-08 11:26:50 +0200

    Added session management

--- app/src/main/java/com/gianlucaparadise/castyourinstructions/MainActivity.kt
+++ app/src/main/java/com/gianlucaparadise/castyourinstructions/MainActivity.kt
@@ -1,23 +1,40 @@
 package com.gianlucaparadise.castyourinstructions
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.cast.framework.CastButtonFactory
-import com.google.android.gms.cast.framework.CastContext
-
+import com.google.android.gms.cast.framework.*
 
 class MainActivity : AppCompatActivity() {
 
     private var mCastContext: CastContext? = null
     private var mediaRouteMenuItem: MenuItem? = null
 
+    private var mCastSession: CastSession? = null
+    private lateinit var mSessionManager: SessionManager
+    private val mSessionManagerListener = SessionManagerListenerImpl()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mCastContext = CastContext.getSharedInstance(this)
+        mSessionManager = CastContext.getSharedInstance(this).sessionManager
+        mCastSession = mCastContext!!.sessionManager.currentCastSession
+    }
+
+    override fun onResume() {
+        mCastSession = mSessionManager.currentCastSession
+        mSessionManager.addSessionManagerListener(mSessionManagerListener, CastSession::class.java)
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mSessionManager.removeSessionManagerListener(mSessionManagerListener, CastSession::class.java)
+        mCastSession = null
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -26,5 +43,49 @@ class MainActivity : AppCompatActivity() {
         mediaRouteMenuItem =
             CastButtonFactory.setUpMediaRouteButton(applicationContext, menu, R.id.media_route_menu_item)
         return true
+    }
+
+    private inner class SessionManagerListenerImpl : SessionManagerListener<CastSession> {
+        private var tag = "SessionManagerListenerImpl"
+
+        override fun onSessionSuspended(p0: CastSession?, p1: Int) {
+            Log.d(tag, "onSessionSuspended")
+        }
+
+        override fun onSessionStarting(p0: CastSession?) {
+            Log.d(tag, "onSessionStarting")
+        }
+
+        override fun onSessionResuming(p0: CastSession?, p1: String?) {
+            Log.d(tag, "onSessionResuming")
+        }
+
+        override fun onSessionEnding(p0: CastSession?) {
+            Log.d(tag, "onSessionEnding")
+        }
+
+        override fun onSessionStartFailed(p0: CastSession?, p1: Int) {
+            Log.d(tag, "onSessionStartFailed")
+        }
+
+        override fun onSessionResumeFailed(p0: CastSession?, p1: Int) {
+            Log.d(tag, "onSessionResumeFailed")
+        }
+
+        override fun onSessionStarted(session: CastSession, sessionId: String) {
+            Log.d(tag, "onSessionStarted")
+            mCastSession = session
+            invalidateOptionsMenu()
+        }
+
+        override fun onSessionResumed(session: CastSession, wasSuspended: Boolean) {
+            Log.d(tag, "onSessionResumed")
+            invalidateOptionsMenu()
+        }
+
+        override fun onSessionEnded(session: CastSession, error: Int) {
+            Log.d(tag, "onSessionEnded")
+            finish()
+        }
     }
 }
