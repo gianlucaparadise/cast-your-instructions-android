f2fa967303fdc48d1c223632db49e4151036a26b
gianlucaparadise <gianlucaparadise@gmail.com>
2019-09-15 18:14:29 +0200

    Added DataBinding for RecipeList

--- app/src/main/java/com/gianlucaparadise/castyourinstructions/adapters/MyRecipeRecyclerViewAdapter.kt
+++ app/src/main/java/com/gianlucaparadise/castyourinstructions/adapters/MyRecipeRecyclerViewAdapter.kt
@@ -4,9 +4,8 @@ package com.gianlucaparadise.castyourinstructions.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.gianlucaparadise.castyourinstructions.R
+import com.gianlucaparadise.castyourinstructions.databinding.FragmentRecipeBinding
 import com.gianlucaparadise.castyourinstructions.fragments.RecipesListFragment.OnListFragmentInteractionListener
 import com.gianlucaparadise.castyourinstructions.models.Recipe
 import kotlinx.android.synthetic.main.fragment_recipe.view.*
@@ -26,42 +25,34 @@ class MyRecipeRecyclerViewAdapter// Notify the active callbacks interface (the a
             this.notifyDataSetChanged()
         }
 
-    private val mOnClickListener: View.OnClickListener
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.fragment_recipe, parent, false)
-        return ViewHolder(view)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = FragmentRecipeBinding.inflate(inflater, parent, false)
+        // val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_recipe, parent, false)
+        return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        if (recipes == null) return
-
-        val item = recipes!![position]
-        holder.mContentView.text = item.title
-
-        with(holder.mView) {
-            tag = item
-            setOnClickListener(mOnClickListener)
-        }
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(recipes?.get(position))
 
     override fun getItemCount(): Int = recipes?.size ?: 0
 
-    inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
-        val mContentView: TextView = mView.content
-
-        override fun toString(): String {
-            return super.toString() + " '" + mContentView.text + "'"
-        }
+    fun onRecipeClicked(v: View) {
+        val item = v.tag as Recipe
+        // Notify the active callbacks interface (the activity, if the fragment is attached to
+        // one) that an item has been selected.
+        mListener?.onListFragmentInteraction(item)
     }
 
-    init {
-        mOnClickListener = View.OnClickListener { v ->
-            val item = v.tag as Recipe
-            // Notify the active callbacks interface (the activity, if the fragment is attached to
-            // one) that an item has been selected.
-            mListener?.onListFragmentInteraction(item)
+    inner class ViewHolder(val binding: FragmentRecipeBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Recipe?) {
+            binding.item = item
+            binding.handlers = this@MyRecipeRecyclerViewAdapter
+            binding.executePendingBindings()
+        }
+
+        override fun toString(): String {
+            return super.toString() + " '" + binding.root.content.text + "'"
         }
     }
 }
